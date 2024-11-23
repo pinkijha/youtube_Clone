2e1e4b362e39429926dd69186bbe986f8992fdbb
Pinki Jha <pinkijha1310@gmail.com>
2024-11-23 17:47:08 +0530

    use Context hook and custome hooks to access the Api and solve the api related problem

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,35 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { fetchData } from '../utils/rapidApi';
+
+export const AuthContext = createContext()
+
+
+export default function AuthProvider ({children})  {
+
+    const [ loading, setLoading ] = useState(false);
+    const  [ value, setValue ] = useState('New');
+    const [ data, setData ] = useState([]);
+
+    useEffect(() => {
+        fetchAllData(value)
+    }, [value]);
+
+    const fetchAllData = (query) =>{
+        setLoading(true);
+        fetchData(`search/?q=${query}`).then((res)=> {
+            setData(res);
+            setLoading(false);
+        })
+    }
+
+  return (
+    <AuthContext.Provider value={{loading, data, value, setValue}}>
+        {children}
+
+    </AuthContext.Provider>
+  )
+}
+
+// export default AuthProvider;
+
+export const useAuth = () => useContext(AuthContext);
--- src/utils/rapidApi.js
+++ src/utils/rapidApi.js
@@ -4,7 +4,6 @@ const API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY;
 const BASE_URL = 'https://youtube138.p.rapidapi.com';
 
 const options = {
-	method: 'GET',
 	headers: {
 		'x-rapidapi-key': API_KEY,
 		'x-rapidapi-host': 'youtube138.p.rapidapi.com'
@@ -12,11 +11,12 @@ const options = {
 };
 
 export const fetchData = async (url) => {
-    try{
-        const {data} = await axios.get( `${BASE_URL}/${url}`, options);
-        return data;
-    }catch(error){
-        console.error("Error fetching Api Data:", error);
-        throw error;
+    try {
+      const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+      return data;
+    } catch (error) {
+      console.error("Error fetching API Data:", error);
+      throw error;
     }
-}
+  };
+  
