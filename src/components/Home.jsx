dd2c61ff1c304788b808870145bf9abfa48b9d4a
Pinki Jha <pinkijha1310@gmail.com>
2024-11-24 00:00:28 +0530

    start editing home page coponent

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import Sidebar from './Sidebar'
 import Video from './Video'
+import { useAuth } from '../context/AuthProvider'
 
 const Home = () => {
+    const { data } = useAuth();
+    console.log(data)
   return (
     <div className='flex'>
       <Sidebar/>
       <div>
-        <Video/>
+        {
+            data.map((item) => {
+                if ()
+                    
+            <Video key={item.id} video={item.video}/>
+            })
+            }
       </div>
     </div>
   )
