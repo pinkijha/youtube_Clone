6c500f4d53a2296f22794b9c59a505688f1e0f86
Pinki Jha <pinkijha1310@gmail.com>
2024-11-27 01:59:16 +0530

    make changes on videoPlaying Components

--- src/components/VideoPlaying.jsx
+++ src/components/VideoPlaying.jsx
@@ -50,72 +50,63 @@ const VideoPlaying = () => {
           </div>
 
           {/* channel logo & title */}
-          {/* <div className="font-bold text-sm md:text-xl mt-4 line-clamp-2">
+            
+          
+          <div className="font-bold text-sm md:text-xl mt-4 line-clamp-2">
             {video?.title}
           </div>
-
-          <div className="flex justify-between flex-col md:flex-row mt-4  ">
+          <div className="flex justify-between flex-col md:flex-row mt-4">
             <div className="flex ">
               <div className="flex items-start">
-                <div className="flex h-11 w-11 rounded-full overflow-hidden ">
+                <div className="flex h-11 w-11 rounded-full overflow-hidden">
                   <img
-                    className="h-full w-full object-cover "
-                    src={video?.author?.avatar[0]?.url}
-                    alt="channel-logo"
+                    className="h-full w-full object-cover"
+                    src={video?.author?.avatar?.[0]?.url}
                   />
                 </div>
               </div>
-
               <div className="flex space-x-5">
                 <div className="flex flex-col ml-3">
                   <div className="text-md font-semibold flex items-center">
                     {video?.author?.title}
                     {video?.author?.badges[0]?.type === "VERIFIED_CHANNEL" && (
-                      <BsFillCheckCircleFill className="text-white/[0.5] ml-1 text-[12px]" />
+                      <BsFillCheckCircleFill className="text-white/[0.5] text-[12px] ml-1" />
                     )}
                   </div>
-
-                  <div className="text-sm">
-                    {video?.author?.stats?.subcribersText}
+                  <div className=" text-sm">
+                    {video?.author?.stats?.subscribersText}
                   </div>
                 </div>
-                <span
-                  className="mt-1 text-center bg-rose-500 px-3 pt-2 rounded-full text-white
-                cursor-pointer hover:bg-red-400"
-                >
+                <span className="mt-1 text-center bg-red-500 px-3 pt-2 rounded-full text-white cursor-pointer hover:bg-red-700 duration-200 ">
                   Subscribe
                 </span>
               </div>
             </div>
-
             <div className="flex mt-4 md:mt-0">
               <div className="flex items-center justify-center h-11 px-6 rounded-3xl bg-white/[0.15]">
                 <AiOutlineLike className="text-xl mr-2" />
-                {`${abbreviateNumber(video?.stats?.likes, 2)}Likes`}
+                {`${abbreviateNumber(video?.stats?.likes, 2)} Likes`}
               </div>
-              <div className="flex items-center justify-center h-11 px-6 rounded-3xl bg-white/[0.15]">
-                {`${abbreviateNumber(video?.stats?.views, 2)}Likes`}
+              <div className="flex items-center justify-center h-11 px-6 rounded-3xl bg-white/[0.15] ml-4">
+                {`${abbreviateNumber(video?.stats?.views, 2)} Views`}
               </div>
             </div>
           </div>
-
-          <div className="p-4 bg-gray-100 rounded-xl mt-4 text-sm">
+          <div className="p-4 bg-gray-100 rounded-xl mt-4 text-sm  line-clamp-6">
             {video?.description}
           </div>
           <div className="flex gap-x-6 font-semibold rounded-xl mt-4 text-xl">
             {video?.stats?.comments} <p>Comments</p>
-          </div> */}
+          </div>
         </div>
-        
-      <div className="flex flex-col px-4 py-6 h-[calc(100vh-4.625rem)] 
-      overflow-y-scroll overflow-x-hidden lg:w-[350px] xl:w-[400px] ">
+        <div className="flex flex-col px-4 py-6 h-[calc(100vh-4.625rem)] 
+              overflow-y-scroll overflow-x-hidden lg:w-[350px] xl:w-[400px] ">
         {relatedVideo?.contents?.map((item, index) => {
           if (item?.type!=='video') return false;
           return <SuggestedVideo key={item.video.videoId || index} video={item?.video} />
 
         })}
       </div>
-
       </div>
     </div>
   );
