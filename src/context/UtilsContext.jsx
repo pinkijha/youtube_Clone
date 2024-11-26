9218e3050111524273b5428e76c9443e5df322dd
Pinki Jha <pinkijha1310@gmail.com>
2024-11-26 16:22:09 +0530

    create new context i.e utilsContext for MenuBar show and hide

---
+++ src/context/UtilsContext.jsx
@@ -0,0 +1,19 @@
+import { createContext, useContext, useState } from "react";
+
+const UtilsContext = createContext();
+
+export const UtilsContextProvider = ({children}) => {
+    const [ isSidebar, setIsSidebar ] = useState(false);
+    const [ mobileShow, setMobileShow ] = useState(false);
+    return <UtilsContext.Provider value={{isSidebar, setIsSidebar, mobileShow, setMobileShow}}>
+        {children}
+    </UtilsContext.Provider>
+}
+
+// custome hooks
+export const useUtils = () => {
+    const utilsContext = useContext(UtilsContext);
+    if(!utilsContext) return null;
+    return utilsContext;
+}
+
